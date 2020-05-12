eafa229158fbcec7a61e16ea908837c959a4a23c
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-05-12 19:08:46 +0100

    Added Serial Version UIDs.

--- src/exceptions/InvalidUserKindException.java
+++ src/exceptions/InvalidUserKindException.java
@@ -2,6 +2,11 @@ package exceptions;
 
 public class InvalidUserKindException extends Exception {
     
+    /**
+     * Serial version UID.
+     */
+    private static final long serialVersionUID = 2247045929902441380L;
+
     /* Exception message */
     private static final String MESSAGE = "%s is an invalid user kind!";
     
--- src/exceptions/UserAlreadyExistsException.java
+++ src/exceptions/UserAlreadyExistsException.java
@@ -2,6 +2,11 @@ package exceptions;
 
 public class UserAlreadyExistsException extends Exception {
     
+    /**
+     * Serial version UID.
+     */
+    private static final long serialVersionUID = 7146754211447615073L;
+
     /* Exception message */
     private static final String MESSAGE = "%s already exists!";
     
