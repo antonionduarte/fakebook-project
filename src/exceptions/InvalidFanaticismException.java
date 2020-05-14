3dc4797996dc5813cd86630a980527340ca95ffe
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-05-14 02:07:22 +0100

    Added InvalidFanaticismException

---
+++ src/exceptions/InvalidFanaticismException.java
@@ -0,0 +1,18 @@
+package exceptions;
+
+public class InvalidFanaticismException extends Exception {
+    
+    /**
+     *
+     */
+    private static final long serialVersionUID = 8486642903904460641L;
+    
+    /* Exception message */
+    private static final String MESSAGE = "Oh please, who would be a fanatic of <%s>?";
+
+    /* Constructor */
+    public InvalidFanaticismException() {
+        super(MESSAGE);
+    }
+    
+}
