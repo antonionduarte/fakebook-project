cbe19b4fad017615880003ba0ca8e1c6e7a3a493
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-05-14 11:03:50 +0100

    Added UID

--- src/exceptions/InvalidFanaticismException.java
+++ src/exceptions/InvalidFanaticismException.java
@@ -6,9 +6,9 @@ public class InvalidFanaticismException extends Exception {
      *
      */
     private static final long serialVersionUID = 8486642903904460641L;
-    
+
     /* Exception message */
-    private static final String MESSAGE = "Oh please, who would be a fanatic of <%s>?";
+    private static final String MESSAGE = "Oh please, who would be a fanatic of <%s>?\n";
 
     /* Constructor */
     public InvalidFanaticismException() {
