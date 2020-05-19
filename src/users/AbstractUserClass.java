bc8baa6762a4f53d5d8cbc04b3e10cb9b0af4ac0
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-05-19 02:17:07 +0100

    Didn't implement much but I did finish re-studying the different types of structures

--- src/users/AbstractUserClass.java
+++ src/users/AbstractUserClass.java
@@ -1,5 +1,81 @@
 package users;
 
 public abstract class AbstractUserClass implements User {
+
+    /* Variables */
+
+    /**
+     * Identifier of the user
+     */
+    private String userId;
+
+    /**
+     * The type of the user (Fanatic, Liar, Naive, Selfcentered)
+     */
+    private String userKind;
+
+    /**
+     * Number of friends that the user has
+     */
+    private int numFriends;
     
+    /**
+     * Number of posts that the user made
+     */
+    private int numPosts;
+
+    /**
+     * Number of comments that the user made
+     */
+    private int numComments;
+
+    /* Constructor */
+    
+    /**
+     * 
+     * @param userId
+     * @param userKind
+     */
+    protected AbstractUserClass(String userId, String userKind) {
+        this.userId = userId;
+        this.userKind = userKind;
+        numFriends = 0;
+        numPosts = 0;
+        numComments = 0;
+    }
+
+    @Override
+    public String getId() {
+        return userId;
+    }
+
+    @Override
+    public String getKind() {
+        return userKind;
+    }
+
+    @Override
+    public int getNumFriends() {
+        return numFriends;
+    }
+
+    @Override
+    public int getNumPosts() {
+        return numPosts;
+    }
+
+    @Override
+    public int getNumComments() {
+        return numComments;
+    }
+
+    public void addFriend(User user2) {
+
+    } 
+
+    public void post(DataStructure postHashtags, String postTruthfulness, String postMessage) {
+
+    }
+
+    public 
 }
