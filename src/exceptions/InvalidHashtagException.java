364b64728280de01022597df16a34cc06aaf5d04
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-05-24 22:29:21 +0100

    Added serial UIDs

--- src/exceptions/InvalidHashtagException.java
+++ src/exceptions/InvalidHashtagException.java
@@ -2,6 +2,11 @@ package exceptions;
 
 public class InvalidHashtagException extends RuntimeException {
     
+    /**
+     * Serial version UID.
+     */
+    private static final long serialVersionUID = 5278952031591834601L;
+
     /* Exception message */
     private static final String MESSAGE = "Oh please, who would write about %s";
     
--- src/exceptions/InvalidNumberOfPostsException.java
+++ src/exceptions/InvalidNumberOfPostsException.java
@@ -2,6 +2,11 @@ package exceptions;
 
 public class InvalidNumberOfPostsException extends RuntimeException {
     
+    /**
+     * Serial version UID.
+     */
+    private static final long serialVersionUID = 970572607340229809L;
+
     /* Exception message */
     private static final String MESSAGE = "Invalid number of posts to present!";
     
