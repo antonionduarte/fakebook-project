4a3a52012f78750a9733af5ad06eefc938ba83e7
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-05-25 01:48:16 +0100

    Added PostComparator (not done yet don't judge me procrastinei OK!?)

---
+++ src/comparators/PostComparator.java
@@ -0,0 +1,12 @@
+package comparators;
+
+import java.util.Comparator;
+
+public class PostComparator<Post> implements Comparator<Post> {
+
+    @Override
+    public int compare(Post post1, Post post2) {
+        return 0;
+    }
+    
+}
