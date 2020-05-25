0155e17dfb3ca331a48fb25f85969c343b64cea9
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-05-25 20:49:50 +0100

    Finished the comparator

---
+++ src/comparators/TopicPostsComparator.java
@@ -0,0 +1,21 @@
+package comparators;
+
+import java.util.Comparator;
+import posts.*;
+
+public class TopicPostsComparator implements Comparator<Post> {
+
+    @Override
+    public int compare(Post post1, Post post2) {
+        if ((post1.getNumComments() - post1.getNumComments()) != 0) {
+            return post2.getNumComments() - post1.getNumComments();
+        }
+        
+        if (post1.getAuthorId().compareTo(post2.getAuthorId()) != 0) {
+            return post1.getAuthorId().compareTo(post2.getAuthorId());
+        }
+
+        return post2.getId() - post1.getId();
+    }
+    
+}
