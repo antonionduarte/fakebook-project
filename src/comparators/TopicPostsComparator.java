fde0c181ae47b38146287e92754d9c2a6e2b74f2
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-05-26 23:35:59 +0100

    Changed order in comparator

--- src/comparators/TopicPostsComparator.java
+++ src/comparators/TopicPostsComparator.java
@@ -7,8 +7,8 @@ public class TopicPostsComparator implements Comparator<Post> {
 
     @Override
     public int compare(Post post1, Post post2) {
-        if ((post1.getNumComments() != post1.getNumComments())) {
-            return post2.getNumComments() - post1.getNumComments();
+        if ((post2.getNumComments() != post1.getNumComments())) {
+            return post1.getNumComments() - post2.getNumComments();
         }
         
         if (post1.getAuthorId().compareTo(post2.getAuthorId()) != 0) {
