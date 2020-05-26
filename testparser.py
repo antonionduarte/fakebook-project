519b678ed1bce534d9578693c6e6c3496c214332
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-05-26 01:53:32 +0100

    Updated parser

--- testparser.py
+++ testparser.py
@@ -4,11 +4,11 @@ NUMBER_OF_TESTS = 19
 
 system("cd src/ ; javac -Xlint -d ../bin Main.java ; cd ..")
 
-COMMAND_RUN = "cd bin ; java Main < ../tests/fakebook%.2din.txt > ../output-log/fakebook%.2dtest_log.txt"
+COMMAND_RUN = "cd bin ; java Main < ../tests/fakebook%.2din.txt > ../log/fakebook%.2dtest_log.txt"
 
 for i in range(0, NUMBER_OF_TESTS):
 	system(COMMAND_RUN % (i+1, i+1))
-	code = system("diff output-log/fakebook%.2dtest_log.txt tests/fakebook%.2dout.txt" % (i + 1, i + 1))
+	code = system("diff log/fakebook%.2dtest_log.txt tests/fakebook%.2dout.txt" % (i + 1, i + 1))
 	if code != 0:
 		print("       FAILED %.2s        " % str(i + 1))
 		exit(0)
